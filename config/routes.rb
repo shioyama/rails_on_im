7953bab0cf2d8fd5fe7f65517961467feeb08559
Chris Salzberg <chris.salzberg@shopify.com>
2023-02-09 10:32:52 +0900

    Modify routes to remove my_app/application prefix

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  scope module: "my_app/application" do
+    # Defines the root path route ("/")
+    root "posts#index"
+  end
 end
